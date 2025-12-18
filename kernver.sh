100699a2612bfcad1d380a008e39dacff42fe31e
ading2210 <ading2019@gmail.com>
2025-12-18 01:06:59 -0500

    add shell script for finding kernel version of images

---
+++ kernver.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+temp_dir="$(mktemp -d)"
+
+clean_up () {
+  local status="$?"
+  rm -rf "$temp_dir"
+  exit "$status"
+} 
+
+get_img_size() {
+  local img_url="$1"
+  curl -s --header "Range: bytes=0-$((1*1024*1024))" "$img_url" \
+    | busybox unzip - -lq 2>/dev/null \
+    | tail -n1 \
+    | cut -d' ' -f1
+}
+
+download_partial() {
+  local img_url="$1"
+  local out_file="$2"
+  curl -s --header "Range: bytes=0-$((100*1024*1024))" "$img_url" \
+    | busybox unzip - -p 2>/dev/null \
+    | dd of="$out_file" bs=1M conv=notrunc status=none
+}
+
+get_kernver() {
+  local img_url="$1"
+  local img_bin="$temp_dir/image.bin"
+  local kernel_bin="$temp_dir/kernel.bin"
+
+  local img_size="$(get_img_size "$img_url")"
+  truncate "$img_bin" -s "$(($img_size*11/10))"
+  download_partial "$img_url" "$img_bin"
+
+  local fdisk_out="$(fdisk -l "$img_bin" 2>/dev/null | grep "${img_bin}4")"
+  local start="$(echo "$fdisk_out" | awk '{print $2}')"
+  local sectors="$(echo "$fdisk_out" | awk '{print $4}')"
+  dd if="$img_bin" of="$kernel_bin" bs=512 skip="$start" count="$sectors" status=none
+
+  futility show "$kernel_bin" | grep "Kernel version:" | awk '{print $3}'
+  rm -f "$img_bin" "$kernel_bin"
+}
+
+trap clean_up EXIT
+
+get_kernver "$1"

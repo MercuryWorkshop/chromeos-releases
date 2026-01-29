f475429c73b80a281b2ef841dc98156103681c40
ading2210 <ading2019@gmail.com>
2026-01-28 23:04:02 -0800

    migrate kernver.json and dates.json to git, set commit dates manually

--- src/git.py
+++ src/git.py
@@ -1,10 +1,12 @@
 import json
-from datetime import datetime
+import shutil
+from datetime import timezone, datetime
 
 from dulwich.object_store import tree_lookup_path
 from dulwich import porcelain
 
 import wayback
+import kernver
 import common
 
 repo_path = common.data_path / "repo"
@@ -36,30 +38,46 @@ def get_git_data():
       data[board_name] = list(filter(lambda x: x["platform_version"] != "0.0.0", images))
     data_sources.append(data)
   return data_sources
-  
+
+def make_commit(repo, path, dt):
+  commit_msg = f"{dt.replace(tzinfo=None)} - Update {path.relative_to(repo_path)}"
+  porcelain.add(repo, path)
+  porcelain.commit(
+    repo, message=commit_msg, author=commit_author, committer=commit_author,
+    commit_timestamp=dt.timestamp(), author_timezone=0, commit_timezone=0
+  )
+
 def migrate_to_git():
   print("Migrating wayback snapshots to git repo...")
   clone_repo()
 
-  wayback_files = []
+  #migrate wayback snapshots
+  migrated_files = []
+  dt_now = datetime.utcnow().replace(microsecond=0, tzinfo=timezone.utc)
   for path in wayback.downloads_path.rglob("*.json"):
     if not path.stem.isdigit():
       continue
-    dt = datetime.strptime(path.stem, "%Y%m%d%H%M%S")
-    wayback_files.append((dt, path))
-  wayback_files.sort()
+    dt = datetime.strptime(path.stem, "%Y%m%d%H%M%S").replace(tzinfo=timezone.utc)
+    relative_path = path.relative_to(wayback.downloads_path)
+    new_path = repo_path / "sources" / f"{relative_path.parent}.json"
+    migrated_files.append((dt, path, new_path))
+  
+  #migrate kernver data
+  new_kerver_path = sources_path / "kernver.json"
+  migrated_files.append((dt_now, kernver.dl_kernver_path, new_kerver_path))
 
+  #migrate recovery image date data
+  new_dates_path = sources_path / "dates.json"
+  migrated_files.append((dt_now, wayback.dl_dates_path, new_dates_path))
+
+  migrated_files.sort()
   print("Creating git commits...")
-  with porcelain.open_repo_closing(repo_path) as repo:
-    for dt, path in wayback_files:
-      relative_path = path.relative_to(wayback.downloads_path)
-      new_path = repo_path / "sources" / f"{relative_path.parent}.json"
-      new_path.parent.mkdir(parents=True, exist_ok=True)
-      new_path.write_bytes(path.read_bytes())
 
-      commit_msg = f"{dt} - Update {new_path.relative_to(sources_path)}"
-      porcelain.add(repo, new_path)
-      porcelain.commit(repo, commit_msg, author=commit_author, committer=commit_author)
+  with porcelain.open_repo_closing(repo_path) as repo:
+    for dt, old_path, new_path in migrated_files:
+      new_path.parent.mkdir(parents=True, exist_ok=True)
+      shutil.copy(old_path, new_path)
+      make_commit(repo, new_path, dt)
   
   print("Done migrating.")
 
